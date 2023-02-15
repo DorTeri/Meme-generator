e1c73ff8854665fda7581d83475962012227a28f
DorTeri <dortayari@gmail.com>
2023-02-15 11:09:30 +0200

    Phase 1 almost done

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,38 @@
 'use strict'
+let gElCanvas
+let gCtx
+let gKeywordSearchCountMap = { 'funny': 12, 'cat': 16, 'baby': 2 }
+
+function onInit() {
+    gElCanvas = document.querySelector('#edit-canvas')
+    gCtx = gElCanvas.getContext('2d')
+    renderMeme()
+}
+
+function renderMeme() {
+    const meme = getMeme()
+    const imgUrl = findUrlById(meme.selectedImgId).url
+    const img = new Image()
+    img.src = imgUrl
+    img.onload = () => {
+        gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
+        drawText(meme.lines[0].txt, 200, 50)
+    }
+}
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = 'white'
+    gCtx.font = '35px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.textBaseline = 'middle'
+
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function onSetLineTxt(txt) {
+    setLineTxt(txt)
+    renderMeme()
+}
--- js/service/service.js
+++ js/service/service.js
@@ -1,1 +1,30 @@
 'use strict'
+
+let gImgs = [
+    { id: 1, url: 'images/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: 'images/2.jpg', keywords: ['funny'] }
+]
+let gMeme = {
+    selectedImgId: 2,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I sometimes eat Falafel',
+            size: 20,
+            align: 'left',
+            color: 'red'
+        }
+    ]
+}
+
+function getMeme() {
+    return gMeme
+}
+
+function findUrlById(id) {
+    return gImgs.find(img => img.id === id)
+}
+
+function setLineTxt(txt) {
+    gMeme.lines[0].txt = txt
+}
