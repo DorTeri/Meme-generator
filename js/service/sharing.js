ddf0e9bce721a29aa89489d9ea3b172d750a37b0
DorTeri <dortayari@gmail.com>
2023-02-18 20:18:22 +0200

    Removed notes

--- js/service/sharing.js
+++ js/service/sharing.js
@@ -1,38 +1,26 @@
 'use strict'
 
 function onUploadImg() {
-    const imgDataUrl = gElCanvas.toDataURL('image/jpeg') // Gets the canvas content as an image format
-
-    // A function to be called if request succeeds
-    function onSuccess(uploadedImgUrl) {
-        // Encode the instance of certain characters in the url
+    const imgDataUrl = gElCanvas.toDataURL('image/jpeg')
         const encodedUploadedImgUrl = encodeURIComponent(uploadedImgUrl)
         console.log(encodedUploadedImgUrl)
         window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodedUploadedImgUrl}&t=${encodedUploadedImgUrl}`)
     }
-    // Send the image to the server
     doUploadImg(imgDataUrl, onSuccess)
 }
 
 function doUploadImg(imgDataUrl, onSuccess) {
-    // Pack the image for delivery
     const formData = new FormData()
     formData.append('img', imgDataUrl)
 
-    // Send a post req with the image to the server
     const XHR = new XMLHttpRequest()
     XHR.onreadystatechange = () => {
-        // If the request is not done, we have no business here yet, so return
+
         if (XHR.readyState !== XMLHttpRequest.DONE) return
-        // if the response is not ok, show an error
+
         if (XHR.status !== 200) return console.error('Error uploading image')
         const { responseText: url } = XHR
-        // Same as
-        // const url = XHR.responseText
 
-        // If the response is ok, call the onSuccess callback function, 
-        // that will create the link to facebook using the url we got
-        console.log('Got back live url:', url)
         onSuccess(url)
     }
     XHR.onerror = (req, ev) => {
@@ -61,14 +49,7 @@ function loadImageFromInput(ev) {
         let img = new Image() 
         img.src = event.target.result 
         gMeme.upload = event.target.result
-        // img.onload = onImageReady.bind(null, img)
-        // Can also do it this way:
         img.onload = () => renderMeme()
     }
-    reader.readAsDataURL(ev.target.files[0]) // Read the file we picked
+    reader.readAsDataURL(ev.target.files[0]) 
 }
-
-// function renderImg(img) {
-//     // Draw the img on the canvas
-//     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-// }
